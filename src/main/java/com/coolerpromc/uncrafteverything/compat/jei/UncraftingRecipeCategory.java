97632f8a54649dc64046da38da05e83c7a1a5809
CoolerProYT <veronlam1818@gmail.com>
2025-06-17 13:11:34 +0800

    1.6.1-Added JEI/REI Compat

---
+++ src/main/java/com/coolerpromc/uncrafteverything/compat/jei/UncraftingRecipeCategory.java
@@ -0,0 +1,49 @@
+package com.coolerpromc.uncrafteverything.compat.jei;
+
+import com.coolerpromc.uncrafteverything.UncraftEverything;
+import com.coolerpromc.uncrafteverything.block.UEBlocks;
+import com.coolerpromc.uncrafteverything.util.JEIUncraftingTableRecipe;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.recipe.types.IRecipeType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("removal")
+public record UncraftingRecipeCategory(IGuiHelper guiHelper) implements IRecipeCategory<JEIUncraftingTableRecipe> {
+    @Override
+    public @NotNull IRecipeType<JEIUncraftingTableRecipe> getRecipeType() {
+        return UEJEIPlugin.UNCRAFTING_TYPE;
+    }
+
+    @Override
+    public @NotNull Component getTitle() {
+        return Component.translatable("block.uncrafteverything.uncrafting_table");
+    }
+
+    @Override
+    public @NotNull IDrawable getBackground() {
+        return guiHelper.createDrawable(ResourceLocation.fromNamespaceAndPath(UncraftEverything.MODID, "textures/gui/uncrafting_table_gui.png"), 20, 15, 137, 56);
+    }
+
+    @Override
+    public @NotNull IDrawable getIcon() {
+        return guiHelper.createDrawableItemStack(new ItemStack(UEBlocks.UNCRAFTING_TABLE.get()));
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder iRecipeLayoutBuilder, JEIUncraftingTableRecipe recipe, @NotNull IFocusGroup iFocusGroup) {
+        iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.INPUT, 6, 20).addItemStack(recipe.getInput());
+        for (int i = 0; i < recipe.getOutputs().size(); i ++){
+            if (recipe.getOutputs().get(i) != null){
+                iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.OUTPUT, 78 + 18 * (i % 3), 2 + (i / 3) * 18).addIngredients(recipe.getOutputs().get(i));
+            }
+        }
+    }
+}
