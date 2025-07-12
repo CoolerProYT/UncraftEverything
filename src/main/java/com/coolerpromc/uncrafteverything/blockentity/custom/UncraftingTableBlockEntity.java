b9168c938eb859934005230af5772df2f9ececc2
CoolerProYT <veronlam1818@gmail.com>
2025-07-12 14:14:28 +0800

    1.6.5-Uncrafting Table now support ItemStack size larger than 99

--- src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
+++ src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
@@ -98,6 +98,11 @@ public class UncraftingTableBlockEntity extends BlockEntity implements MenuProvi
                 PacketDistributor.sendToPlayersNear((ServerLevel) level, null, getBlockPos().getX(), getBlockPos().getY(), getBlockPos().getZ(), 10, new UncraftingTableDataPayload(getBlockPos(), new ArrayList<>(currentRecipes)));
             }
         }
+
+        @Override
+        public int getSlotLimit(int slot) {
+            return getStackInSlot(slot).getMaxStackSize();
+        }
     };
 
     private final ItemStackHandler outputHandler = new ItemStackHandler(9){
@@ -115,6 +120,11 @@ public class UncraftingTableBlockEntity extends BlockEntity implements MenuProvi
         @Override
         public boolean isItemValid(int slot, @NotNull ItemStack stack) {
             return false;
+        }
+
+        @Override
+        public int getSlotLimit(int slot) {
+            return getStackInSlot(slot).getMaxStackSize();
         }
     };
 
