94ce1c014e397606c8a70c071ccbbc97db17cdce
CoolerProYT <veronlam1818@gmail.com>
2025-07-12 14:14:28 +0800

    1.6.5-Uncrafting Table now support ItemStack size larger than 99

--- src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
+++ src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
@@ -8,6 +8,7 @@ import com.coolerpromc.uncrafteverything.networking.UncraftingTableDataPayload;
 import com.coolerpromc.uncrafteverything.screen.custom.UncraftingTableMenu;
 import com.coolerpromc.uncrafteverything.util.UETags;
 import com.coolerpromc.uncrafteverything.util.UncraftingTableRecipe;
+import com.dplayend.stackableitems.handler.HandlerConfig;
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
@@ -49,6 +50,7 @@ import net.minecraftforge.network.PacketDistributor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Field;
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -96,6 +98,28 @@ public class UncraftingTableBlockEntity extends BlockEntity implements MenuProvi
                 UncraftingTableDataPayload.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), new UncraftingTableDataPayload(getBlockPos(), new ArrayList<>(currentRecipes)));
             }
         }
+
+        @Override
+        public int getSlotLimit(int slot) {
+            String stackableItems = "com.dplayend.stackableitems.handler.HandlerConfig$Server";
+            String fieldName = "defaultStackSize";
+            try{
+                Class<?> handlerConfigClass = Class.forName("com.dplayend.stackableitems.handler.HandlerConfig");
+
+                Field serverField = handlerConfigClass.getDeclaredField("SERVER");
+                serverField.setAccessible(true);
+                Object serverInstance = serverField.get(null); // because it's static
+
+                Class<?> serverClass = serverField.getType();
+                Field defaultStackSizeField = serverClass.getDeclaredField("defaultStackSize");
+                defaultStackSizeField.setAccessible(true);
+                Object value = defaultStackSizeField.get(serverInstance);
+
+                return (int) value;
+            } catch (Exception e) {
+                return super.getSlotLimit(slot);
+            }
+        }
     };
 
     private final ItemStackHandler outputHandler = new ItemStackHandler(9){
@@ -113,6 +137,28 @@ public class UncraftingTableBlockEntity extends BlockEntity implements MenuProvi
         @Override
         public boolean isItemValid(int slot, @NotNull ItemStack stack) {
             return false;
+        }
+
+        @Override
+        public int getSlotLimit(int slot) {
+            String stackableItems = "com.dplayend.stackableitems.handler.HandlerConfig$Server";
+            String fieldName = "defaultStackSize";
+            try{
+                Class<?> handlerConfigClass = Class.forName("com.dplayend.stackableitems.handler.HandlerConfig");
+
+                Field serverField = handlerConfigClass.getDeclaredField("SERVER");
+                serverField.setAccessible(true);
+                Object serverInstance = serverField.get(null); // because it's static
+
+                Class<?> serverClass = serverField.getType();
+                Field defaultStackSizeField = serverClass.getDeclaredField("defaultStackSize");
+                defaultStackSizeField.setAccessible(true);
+                Object value = defaultStackSizeField.get(serverInstance);
+
+                return (int) value;
+            } catch (Exception e) {
+                return super.getSlotLimit(slot);
+            }
         }
     };
 
