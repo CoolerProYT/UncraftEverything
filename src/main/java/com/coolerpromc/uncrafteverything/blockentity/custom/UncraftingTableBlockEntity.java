4d485e1a8dec975f11f5bf3973a260981556266b
CoolerProYT <veronlam1818@gmail.com>
2025-07-22 14:29:46 +0800

    1.6.7

--- src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
+++ src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
@@ -288,6 +288,7 @@ public class UncraftingTableBlockEntity extends TileEntity implements INamedCont
                 || UncraftEverythingConfig.CONFIG.isItemWhitelisted(inputHandler.getStackInSlot(0))
                 || (!UncraftEverythingConfig.CONFIG.isEnchantedItemsAllowed(inputHandler.getStackInSlot(0)) && inputStack.getTag() != null && !inputStack.getTag().contains("Trim"))
                 || (inputStack.getItem() == Items.SHULKER_BOX && inputStack.hasTag())
+                || (inputStack.getItem() == Items.ENCHANTED_BOOK)
         ) {
             if (inputHandler.getStackInSlot(0).getDamageValue() > 0 && !UncraftEverythingConfig.CONFIG.allowDamaged()){
                 this.status = DAMAGED_ITEM;
@@ -309,7 +310,7 @@ public class UncraftingTableBlockEntity extends TileEntity implements INamedCont
                 this.status = SHULKER_WITH_ITEM;
             }
 
-            if (inputHandler.getStackInSlot(0).isEmpty()){
+            if (inputHandler.getStackInSlot(0).isEmpty() || inputHandler.getStackInSlot(0).getItem() == Items.ENCHANTED_BOOK) {
                 this.status = NO_RECIPE;
             }
 
