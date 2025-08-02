bce62d2044164b31a301701865d0573dc61cd454
CoolerProYT <veronlam1818@gmail.com>
2025-08-02 13:02:40 +0800

    1.6.8 - Updated config screen ui

---
+++ src/main/java/com/coolerpromc/uncrafteverything/screen/custom/AbstractScrollableScreen.java
@@ -0,0 +1,76 @@
+package com.coolerpromc.uncrafteverything.screen.custom;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public abstract class AbstractScrollableScreen extends Screen {
+    private static final Identifier SCROLLER_SPRITE = Identifier.of("widget/scroller");
+    private static final Identifier SCROLLER_BACKGROUND_SPRITE = Identifier.of("widget/scroller_background");
+
+    protected int contentHeight;
+    protected boolean scrolling = false;
+    protected double scrollAmount = 0.0;
+
+    protected AbstractScrollableScreen(Text title, int contentHeight) {
+        super(title);
+        this.contentHeight = contentHeight;
+    }
+
+    protected void renderScrollbar(DrawContext guiGraphics, int footerHeight) {
+        int i = this.scrollBarX();
+        int j = this.scrollerHeight();
+        int k = this.scrollBarY();
+        if (getMaxScroll() > 0){
+            guiGraphics.drawGuiTexture(SCROLLER_BACKGROUND_SPRITE, i, 25, 6, this.height - footerHeight);
+            guiGraphics.drawGuiTexture(SCROLLER_SPRITE, i, k, 6, j);
+        }
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (keyCode == 264) { // Down arrow
+            mouseScrolled(0, 0, 0, -1);
+            return true;
+        } else if (keyCode == 265) { // Up arrow
+            mouseScrolled(0, 0, 0, 1);
+            return true;
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (mouseX >= scrollBarX() && mouseY >= scrollBarY() & mouseY <= scrollBarY() + scrollerHeight()) {
+            this.scrolling = true;
+        }
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        this.scrolling = false;
+        return super.mouseReleased(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double dragX, double dragY) {
+        if (this.scrolling) {
+            mouseScrolled(mouseX, mouseY, 0, -(dragY / 4));
+        }
+        return super.mouseDragged(mouseX, mouseY, button, dragX, dragY);
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double horizontalAmount, double verticalAmount){
+        double newScroll = scrollAmount - verticalAmount * 10;
+        scrollAmount = Math.max(0, Math.min(newScroll, getMaxScroll()));
+        return super.mouseScrolled(mouseX, mouseY, horizontalAmount, verticalAmount);
+    }
+
+    protected abstract int scrollBarX();
+    protected abstract int scrollBarY();
+    protected abstract int scrollerHeight();
+    protected abstract int getMaxScroll();
+}
