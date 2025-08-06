390fb358454efb02a53a9316536f543e1a12681e
CoolerProYT <veronlam1818@gmail.com>
2025-08-06 14:53:44 +0800

    1.7.0 - Added configuration option to prevent items from selected mods from being returned during uncrafting. This helps avoid potential issues such as "packet failed to send" errors caused by excessive recipe combinations, and also prevents possible exploits.

--- src/main/java/com/coolerpromc/uncrafteverything/screen/custom/UEConfigScreen.java
+++ src/main/java/com/coolerpromc/uncrafteverything/screen/custom/UEConfigScreen.java
@@ -32,13 +32,15 @@ public class UEConfigScreen extends AbstractScrollableScreen {
     private boolean allowUnsmithing = config.allowUnsmithing();
     private boolean allowDamagedItems = config.allowDamaged();
     private boolean preventModdedIngredientsFromVanillaItems = config.preventModdedIngredientsFromVanillaItems();
+    private List<String> restrictedModIngredients = config.restrictedModIngredients();
 
     private MultiLineEditBox restrictionsInput;
     private EditBox experienceInput;
+    private MultiLineEditBox restrictedModInput;
     private Button saveButton;
 
     protected UEConfigScreen(Component title, Screen parent) {
-        super(title, 245);
+        super(title, 338);
         this.parent = parent;
     }
 
@@ -102,6 +104,12 @@ public class UEConfigScreen extends AbstractScrollableScreen {
         }).bounds(x, (int) (baseY + 245 - scrollAmount), widgetWidth, 20).build();
         this.addRenderableWidget(togglePreventModdedIngredientsFromVanillaItems);
 
+        // Restricted Mod input box
+        String joinedMod = String.join("\n", restrictedModIngredients);
+        restrictedModInput = new MultiLineEditBox(font, x, (int) (baseY + 270 - scrollAmount), widgetWidth, 88, Component.translatable("screen.uncrafteverything.blank"), Component.translatable("screen.uncrafteverything.blank"));
+        restrictedModInput.setValue(joinedMod);
+        this.addRenderableWidget(restrictedModInput);
+
         // Save button
         saveButton = Button.builder(Component.translatable("screen.uncrafteverything.save"), this::pressSaveButton).bounds(this.width / 2 - 100, (this.height - 45) + 15, 200, 20).build();
         this.addRenderableWidget(saveButton);
@@ -158,6 +166,9 @@ public class UEConfigScreen extends AbstractScrollableScreen {
 
         Component preventModded = Component.translatable("screen.uncrafteverything.config.prevent_modded_ingredients_from_vanilla_items_label");
         pGuiGraphics.drawWordWrap(this.font, preventModded, x, (int) (baseY + 245 - scrollAmount + (this.font.lineHeight / 2d) + 1 - this.font.wordWrapHeight(preventModded, textWidth) / 4d), textWidth, 0xFFFFFFFF);
+
+        Component restrictedMod = Component.translatable("screen.uncrafteverything.config.prevent_modid");
+        pGuiGraphics.drawWordWrap(this.font, restrictedMod, x, (int) (baseY + 304 - scrollAmount + (this.font.lineHeight / 2d) + 1 - this.font.wordWrapHeight(restrictedMod, textWidth) / 4d), textWidth, 0xFFFFFFFF);
 
         pGuiGraphics.disableScissor();
 
@@ -242,8 +253,9 @@ public class UEConfigScreen extends AbstractScrollableScreen {
     private void pressSaveButton(Button button){
         restrictions = Arrays.stream(restrictionsInput.getValue().split("\n")).map(String::trim).filter(s -> !s.isEmpty()).toList();
         experience = Integer.parseInt(experienceInput.getValue());
+        restrictedModIngredients = Arrays.stream(restrictedModInput.getValue().split("\n")).map(String::trim).filter(s -> !s.isEmpty()).toList();
 
-        UEConfigPayload configPayload = new UEConfigPayload(restrictionType, restrictions, allowEnchantedItems, experienceType, experience, allowUnsmithing, allowDamagedItems, preventModdedIngredientsFromVanillaItems);
+        UEConfigPayload configPayload = new UEConfigPayload(restrictionType, restrictions, allowEnchantedItems, experienceType, experience, allowUnsmithing, allowDamagedItems, preventModdedIngredientsFromVanillaItems, restrictedModIngredients);
         UEConfigPayload.INSTANCE.send(configPayload, PacketDistributor.SERVER.noArg());
         RequestConfigPayload.INSTANCE.send(new RequestConfigPayload(), PacketDistributor.SERVER.noArg());
         this.getMinecraft().setScreen(parent);
