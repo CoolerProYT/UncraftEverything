a9ae593e5b5e02b1479e65ce1c8157a12509805b
CoolerProYT <veronlam1818@gmail.com>
2025-08-06 14:53:44 +0800

    1.7.0 - Added configuration option to prevent items from selected mods from being returned during uncrafting. This helps avoid potential issues such as "packet failed to send" errors caused by excessive recipe combinations, and also prevents possible exploits.

--- src/main/java/com/coolerpromc/uncrafteverything/networking/ServerPayloadHandler.java
+++ src/main/java/com/coolerpromc/uncrafteverything/networking/ServerPayloadHandler.java
@@ -69,6 +69,7 @@ public class ServerPayloadHandler {
                 config.allowUnSmithing.set(payload.allowUnsmithing());
                 config.allowDamaged.set(payload.allowDamaged());
                 config.preventModdedIngredientsFromVanillaItems.set(payload.preventModdedIngredientsFromVanillaItems());
+                config.restrictedModIngredients.set(payload.restrictedModIngredients());
                 UncraftEverythingConfig.CONFIG_SPEC.save();
             }
         }).exceptionally(e -> {
@@ -90,7 +91,8 @@ public class ServerPayloadHandler {
                         config.allowUnSmithing.get(),
                         config.allowDamaged.get(),
                         config.preventModdedIngredientsFromVanillaItems.get(),
-                        PerItemExpCostConfig.getPerItemExp()
+                        PerItemExpCostConfig.getPerItemExp(),
+                        (List<String>) config.restrictedModIngredients.get()
                 );
                 ResponseConfigPayload.INSTANCE.send(PacketDistributor.PLAYER.with(() -> context.get().getSender()), configPayload);
             }
