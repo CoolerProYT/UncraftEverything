e25197e654cd9d0efcc1228f24772a54e5de028d
CoolerProYT <veronlam1818@gmail.com>
2025-09-08 17:45:23 +0800

    Added proper capability to Uncrafting Table (Down for output, other side for input)

--- src/main/java/com/coolerpromc/uncrafteverything/event/UEModBusEvent.java
+++ src/main/java/com/coolerpromc/uncrafteverything/event/UEModBusEvent.java
@@ -1,15 +1,29 @@
 package com.coolerpromc.uncrafteverything.event;
 
 import com.coolerpromc.uncrafteverything.UncraftEverything;
+import com.coolerpromc.uncrafteverything.blockentity.UEBlockEntities;
 import com.coolerpromc.uncrafteverything.networking.*;
+import net.minecraft.core.Direction;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
 @EventBusSubscriber(modid = UncraftEverything.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class UEModBusEvent {
+    @SubscribeEvent
+    public static void onRegisterCapabilities(RegisterCapabilitiesEvent event) {
+        event.registerBlockEntity(Capabilities.ItemHandler.BLOCK, UEBlockEntities.UNCRAFTING_TABLE_BE.get(), (blockEntity, direction) -> {
+            if (direction == Direction.DOWN){
+                return blockEntity.getOutputHandler();
+            }
+            return blockEntity.getInputHandler();
+        });
+    }
+
     @SubscribeEvent
     public static void registerPayloads(final RegisterPayloadHandlersEvent event) {
         final PayloadRegistrar registrar = event.registrar("1");
