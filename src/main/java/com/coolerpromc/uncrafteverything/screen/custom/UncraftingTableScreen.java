86998914bbaecf056a16b8dce302cb5a52f44102
CoolerProYT <veronlam1818@gmail.com>
2025-09-10 22:43:33 +0800

    [1.8.0] Part 2 - Fixed dragging stack gone when Uncrafting Table Screen closed - Relocated status texts - Fixed possible duplication/ghost stack when quickly opening and closing config screens

--- src/main/java/com/coolerpromc/uncrafteverything/screen/custom/UncraftingTableScreen.java
+++ src/main/java/com/coolerpromc/uncrafteverything/screen/custom/UncraftingTableScreen.java
@@ -7,6 +7,7 @@ import com.coolerpromc.uncrafteverything.networking.UncraftingRecipeSelectionPay
 import com.coolerpromc.uncrafteverything.networking.UncraftingTableCraftButtonClickPayload;
 import com.coolerpromc.uncrafteverything.screen.widget.RecipeSelectionButton;
 import com.coolerpromc.uncrafteverything.util.UncraftingTableRecipe;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
@@ -138,7 +139,6 @@ public class UncraftingTableScreen extends AbstractContainerScreen<UncraftingTab
         this.drawCenteredWordWrapWithoutShadow(pGuiGraphics, this.font, exp, 0, 0, 0xFF00AA00);
         pGuiGraphics.pose().popMatrix();
 
-
         int x = this.leftPos;
         int y = this.topPos;
         int maxPageCount = (int) Math.ceil((double) recipeSize / MAX_PAGE_SIZE);
@@ -255,7 +255,7 @@ public class UncraftingTableScreen extends AbstractContainerScreen<UncraftingTab
                 case 1 -> "screen.uncrafteverything.no_suitable_output_slot";
                 case 2 -> "screen.uncrafteverything.not_enough_exp";
                 case 3 -> "screen.uncrafteverything.not_enough_input";
-                case 4 -> "not_empty_shulker";
+                case 4 -> "screen.uncrafteverything.not_empty_shulker";
                 case 5 -> "screen.uncrafteverything.restricted_by_config";
                 case 6 -> "screen.uncrafteverything.damaged_item";
                 case 7 -> "screen.uncrafteverything.enchanted_item";
@@ -264,30 +264,32 @@ public class UncraftingTableScreen extends AbstractContainerScreen<UncraftingTab
                 default -> "screen.uncrafteverything.blank";
             });
 
-            int textY = y;
+            int textY = y + 55;
 
-            pGuiGraphics.pose().pushMatrix();
-            pGuiGraphics.pose().translate(0, 0);
-            pGuiGraphics.fill(x + 97, y + 16, x + 151, y + 70, 0xAA8B8B8B);
-            pGuiGraphics.pose().popMatrix();
-            List<FormattedCharSequence> formattedText = font.split(FormattedText.of(statusText.getString()), 54);
+            float scale = 0.75f;
+            int boxWidth = 52;
+            int boxLeft = x + 9;
+            float boxCenterX = boxLeft + boxWidth / 2f;
+
+            List<FormattedCharSequence> formattedText = font.split(FormattedText.of(statusText.getString()), (int) (boxWidth * 1.3));
 
             switch (formattedText.size()){
-                case 1 -> textY += 38;
-                case 2 -> textY += 34;
-                case 3 -> textY += 30;
-                case 4 -> textY += 23;
-                default -> textY += 27;
+                case 1 -> textY += 14;
+                case 2 -> textY += 9;
+                default -> textY += 5;
             }
 
-            for (FormattedCharSequence formattedcharsequence : formattedText) {
-                int textWidth = font.width(formattedcharsequence);
-                int centeredX = x + 97 + (54 - textWidth) / 2;
+            for (FormattedCharSequence line : formattedText) {
+                float rawWidth = font.width(line);
+                float drawX = -rawWidth / 2f;
+
                 pGuiGraphics.pose().pushMatrix();
-                pGuiGraphics.pose().translate(centeredX, textY);
-                pGuiGraphics.drawString(font, formattedcharsequence, 0, 0, 0xFFAA0000, false);
+                pGuiGraphics.pose().translate(boxCenterX, textY);
+                pGuiGraphics.pose().scale(scale, scale);
+                pGuiGraphics.drawString(font, line, Math.round(drawX), 0, 0xFFFF5555, false);
                 pGuiGraphics.pose().popMatrix();
-                textY += 9;
+
+                textY += 7;
             }
         }
 
