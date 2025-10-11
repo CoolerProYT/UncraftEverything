2556ddfd2444fd939ad430b3b65425dde99dc5a0
CoolerProYT <veronlam1818@gmail.com>
2025-10-11 15:29:32 +0800

    1.9.0 hotfix

--- src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
+++ src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
@@ -333,7 +333,7 @@ public class UncraftingTableBlockEntity extends BlockEntity implements MenuProvi
                 if (shapelessRecipe.result.getItem() == inputStack.getItem() && inputStack.getCount() < shapelessRecipe.result.getCount()){
                     this.status = NO_ENOUGH_INPUT;
                 }
-                if (inputStack.get(DataComponents.ENCHANTMENTS) != ItemEnchantments.EMPTY){
+                if (inputStack.get(DataComponents.ENCHANTMENTS) != ItemEnchantments.EMPTY && UncraftEverythingConfig.CONFIG.outputEnchantedBook()){
                     return false;
                 }
                 return shapelessRecipe.result.getItem() == inputStack.getItem() && inputStack.getCount() >= shapelessRecipe.result.getCount();
@@ -347,7 +347,7 @@ public class UncraftingTableBlockEntity extends BlockEntity implements MenuProvi
                 if (!UncraftEverythingConfig.CONFIG.allowUnSmithing()){
                     return false;
                 }
-                if (inputStack.get(DataComponents.ENCHANTMENTS) != ItemEnchantments.EMPTY){
+                if (inputStack.get(DataComponents.ENCHANTMENTS) != ItemEnchantments.EMPTY && UncraftEverythingConfig.CONFIG.outputEnchantedBook()){
                     return false;
                 }
                 return inputStack.is(smithingTransformRecipe.result.getItem());
