0ad95cee90b6e5b51f78e38c9b6e8693d3b6139a
CoolerProYT <veronlam1818@gmail.com>
2025-10-24 00:42:18 +0800

    - Fixed page sometimes not updating when uncrafted some items

--- src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
+++ src/main/java/com/coolerpromc/uncrafteverything/blockentity/custom/UncraftingTableBlockEntity.java
@@ -99,7 +99,12 @@ public class UncraftingTableBlockEntity extends BlockEntity implements ExtendedS
                 }
                 currentStack = this.getStack(0);
                 world.updateListeners(pos, getCachedState(), getCachedState(), 3);
-                ServerPlayNetworking.send(player, new UncraftingTableDataPayload(getPos(), new ArrayList<>(currentRecipes.subList(currentRecipes.isEmpty() ? 0 : page * 7, Math.min(page * 7 + 7, currentRecipes.size()))), currentRecipes.size()));
+                int fromIndex = page * 7;
+                if (fromIndex >= currentRecipes.size()) {
+                    fromIndex = currentRecipes.size();
+                }
+                int toIndex = Math.min(fromIndex + 7, currentRecipes.size());
+                ServerPlayNetworking.send(player, new UncraftingTableDataPayload(getPos(), new ArrayList<>(currentRecipes.subList(fromIndex, toIndex)), currentRecipes.size()));
                 player.currentScreenHandler.sendContentUpdates();
             }
         }
